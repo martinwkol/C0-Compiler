fda2f66211f3692b4db7641744ade850e80a5fdd
Martin Koloseus <mkoloseus@posteo.de>
2025-10-23 00:05:17 +0200

    feat(Phi): stores simplified version (if phi is trivial)

--- src/main/java/edu/kit/kastel/vads/compiler/ir/node/Phi.java
+++ src/main/java/edu/kit/kastel/vads/compiler/ir/node/Phi.java
@@ -1,8 +1,13 @@
 package edu.kit.kastel.vads.compiler.ir.node;
 
+import org.jspecify.annotations.Nullable;
+
 import java.util.List;
 
 public final class Phi extends Node {
+    @Nullable
+    private Node simplified = null;
+
     public Phi(Block block) {
         super(block);
     }
@@ -13,5 +18,21 @@ public final class Phi extends Node {
 
     public List<? extends Node> operands() {
         return this.predecessors();
+    }
+
+    public void setSimplifiedVersion(Node node) {
+        simplified = node;
+    }
+
+    public boolean hasSimplifiedVersion() {
+        return simplified != null;
+    }
+    
+    public Node simplified() {
+        assert this.simplified != null;
+        while (this.simplified instanceof Phi phi && phi.hasSimplifiedVersion()) {
+            this.simplified = phi;
+        }
+        return this.simplified;
     }
 }
